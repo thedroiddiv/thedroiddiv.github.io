9b03132f685db38befb90870aa21ab75a2c032f6
Divyansh Kushwaha <divyanshdxn@gmail.com>
2023-07-02 23:52:19 +0530

    feat: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { Navbar } from "./sections/header/Header";
+import { Home } from "./sections/home/Home";
+import ThemeContext from "./theme/ThemeContext";
+import useDarkMode from "./theme/useDarkMode";
+
 function App() {
 
+  const [darkMode, setDarkMode] = useDarkMode();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello there!!
-    </h1>
+    <ThemeContext.Provider
+      value={{
+        darkMode: darkMode,
+        setDarkMode: setDarkMode,
+      }}
+    >
+      <main className="w-screen bg-[#F6F4F6] text-[#252326] dark:bg-[#252326] dark:text-[#F6F4F6]">
+        <Navbar />
+        <div className="w-screen md:px-32 flex flex-col items-center">
+          <Home />
+        </div>
+      </main>
+    </ThemeContext.Provider>
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'pacifico' : ['Pacifico', 'cursive'],
+        'mosk' : ['Mosk', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
+  darkMode: 'class',
 }
 
