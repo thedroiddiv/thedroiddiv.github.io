6e9cec87ee69e697563f9ff7a4c65af224356509
Divyansh Kushwaha <divyanshdxn@gmail.com>
2023-07-03 14:27:35 +0530

    Revert "feat: projects contents"
    This reverts commit 0ee47957d3e622aa057dbd4022818fb2cd957cc1.

--- src/sections/projects/ProjectCard.tsx
+++ src/sections/projects/ProjectCard.tsx
@@ -1,31 +1,19 @@
-import React, { ReactNode } from "react"
-
-interface Props {
-    title: string,
-    desc: string,
-    tech_stack: string[],
-    preview: string[], // [name, url]
-    github: string[] // [name, url]
-}
-
-export const ProjectCard: React.FC<Props> = ({ title, desc, tech_stack, preview, github }) => {
-    return <div className="h-min w-full rounded-2xl border-2 border-[#252326] dark:border-white bg-white dark:bg-transparent">
-        
+export const ProjectCard = () => {
+    return <div className="rounded-2xl border-2 border-[#252326] dark:border-white bg-white dark:bg-transparent">
         <div className="flex flex-row gap-x-2 p-2">
-            <div className="w-4 h-4 rounded-full bg-[#252326] dark:bg-white"></div>
-            <div className="w-4 h-4 rounded-full bg-[#252326] dark:bg-white"></div>
-            <div className="w-4 h-4 rounded-full bg-[#252326] dark:bg-white"></div>
+            <div className="w-5 h-5 rounded-full bg-[#252326] dark:bg-white"></div>
+            <div className="w-5 h-5 rounded-full bg-[#252326] dark:bg-white"></div>
+            <div className="w-5 h-5 rounded-full bg-[#252326] dark:bg-white"></div>
         </div>
 
         <hr className="bg-white border border-[#252326] dark:border-white" />
         <div className="px-4 py-8">
-            <h3 className="text-2xl font-bold">{title}</h3>
-            <p className="mt-4 font-light leading-normal">{desc}</p>
+            <h3 className="text-2xl font-bold">The many ways to change an SVG fill on hover</h3>
+            <p className="mt-4 font-light leading-normal">How do you go about adding a colored hover effect to an icon if it's not an inline SVG? Let's dive into what's possible with CSS and SVG filters.</p>
             <div className="flex flex-row justify-between mt-6">
-                <a href={preview[1]} className="text-xl font-semibold">{preview[0]}</a>
-                <a href={github[1]} className="text-xl font-semibold">{github[0]}</a>
+                <a href="" className="text-xl font-semibold">Button 1</a>
+                <a href="" className="text-xl font-semibold">GitHub</a>
             </div>
         </div>
-
     </div>
 }
